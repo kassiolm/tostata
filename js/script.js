bf83cd9fa92dd6c16a7255f09ede13c84e5ccc3f
unknown <kassiolm@gmail.com>
2026-06-09 10:27:59 -0300

    aumenta area da logo na barra superior em 50%

--- js/script.js
+++ js/script.js
@@ -1422,7 +1422,7 @@ function uploadLogo(ev){
   reader.onload = function(e){
     const img = new Image();
     img.onload = function(){
-      const maxW = 234, maxH = 65;
+      const maxW = 351, maxH = 98;
       let w = img.width, h = img.height;
       if(w > maxW){ h = h * maxW / w; w = maxW; }
       if(h > maxH){ w = w * maxH / h; h = maxH; }
